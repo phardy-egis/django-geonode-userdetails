2914bce06f3303368a794dbaf1d3dfd1e767dd64
p.hardy <pierre.hardy@egis-group.com>
2023-05-11 10:07:02 +0200

    feat: user details view added

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import current_user
 
 urlpatterns = [
+    path('', current_user, name='userdetails')
 ]
 
--- views.py
+++ views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# This view returns the current user details
+@api_view(['GET'])
+def current_user(request):
+    user = request.user
+    if request.user.is_authenticated:
+        return Response({
+            'auth': 1,
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+        })
+    else:
+        return Response({
+            'auth': 0,
+        })
