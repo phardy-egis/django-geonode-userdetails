9634e4fc991101105a8f86ff43c7033e30f06aee
p.hardy <pierre.hardy@egis-group.com>
2023-06-02 11:52:38 +0200

    refactor: remocal of missing dependencies

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.exceptions import PermissionDenied
 
 # This view returns the current user details
 @api_view(['GET'])
